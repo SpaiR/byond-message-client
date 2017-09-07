043334a79f1f37a3d1ef50fec062e1c1469e0bd6
SpaiR <klimbetoo@gmail.com>
2017-09-07 15:54:49 +0300

    Removing of redundant method delegation

--- src/main/java/io/github/spair/byond/message/client/ByteArrayConverter.java
+++ src/main/java/io/github/spair/byond/message/client/ByteArrayConverter.java
@@ -3,15 +3,11 @@ package io.github.spair.byond.message.client;
 class ByteArrayConverter {
 
     byte[] convertIntoBytes(String message) {
-        return parseMessage(message);
-    }
+        char messageSize = (char) (message.getBytes().length + 6);
 
-    private byte[] parseMessage(String message) {
-        final char messageSize = (char) (message.getBytes().length + 6);
-
-        final char[] firstPart = new char[] {0x00, 0x83, 0x00, messageSize, 0x00, 0x00, 0x00, 0x00, 0x00};
-        final char[] messagePart = message.toCharArray();
-        final char[] lastPart = new char[] {0x00};
+        char[] firstPart = new char[] {0x00, 0x83, 0x00, messageSize, 0x00, 0x00, 0x00, 0x00, 0x00};
+        char[] messagePart = message.toCharArray();
+        char[] lastPart = new char[] {0x00};
 
         return convertToByteArray(concatAllParts(firstPart, messagePart, lastPart));
     }
@@ -22,7 +18,7 @@ class ByteArrayConverter {
             resultLength += array.length;
         }
 
-        final char[] result = new char[resultLength];
+        char[] result = new char[resultLength];
 
         int offset = 0;
         for (char[] array : arrays) {
@@ -34,7 +30,7 @@ class ByteArrayConverter {
     }
 
     private byte[] convertToByteArray(char[] charArray) {
-        final byte[] result = new byte[charArray.length];
+        byte[] result = new byte[charArray.length];
 
         for (int i = 0; i < result.length; i++) {
             result[i] = (byte) charArray[i];
