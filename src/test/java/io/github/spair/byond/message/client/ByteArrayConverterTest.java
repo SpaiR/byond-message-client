9fffcbf5d3a0cc6903963d6933a72e49fc047c70
SpaiR <klimbetoo@gmail.com>
2017-09-14 13:44:55 +0300

    added test for 'concatAllParts' method

--- src/test/java/io/github/spair/byond/message/client/ByteArrayConverterTest.java
+++ src/test/java/io/github/spair/byond/message/client/ByteArrayConverterTest.java
@@ -2,6 +2,8 @@ package io.github.spair.byond.message.client;
 
 import org.junit.Test;
 
+import java.lang.reflect.Method;
+
 import static org.junit.Assert.assertArrayEquals;
 
 public class ByteArrayConverterTest {
@@ -11,5 +13,18 @@ public class ByteArrayConverterTest {
         byte[] controlByteArray = new byte[]{0, -125, 0, 22, 0, 0, 0, 0, 0, 83, 112, 97, 99, 101, 32, 83, 116, 97, 116, 105, 111, 110, 32, 49, 51, 0};  // Space Station 13
 
         assertArrayEquals(controlByteArray, ByteArrayConverter.convertIntoBytes("Space Station 13"));
+    }
+
+    @Test
+    public void testConcatAllParts() throws Exception {
+        ByteArrayConverter byteArrayConverter = new ByteArrayConverter();
+
+        Method method = ByteArrayConverter.class.getDeclaredMethod("concatAllParts", char[][].class);
+        method.setAccessible(true);
+
+        char[] controlArray = new char[]{'a', 'b', 'c', 'd', 'e', 'f'};
+        char[] arrayToTest = (char[]) method.invoke(byteArrayConverter, (Object) new char[][]{{'a', 'b', 'c'}, {'d', 'e', 'f'}});
+
+        assertArrayEquals(controlArray, arrayToTest);
     }
 }
